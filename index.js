cf573cc3aafcd1fab93036dd60eb4fe6cb1e7c8d
simplejeff <jeffshen86@gmail.com>
2016-07-25 22:39:47 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = Bear;
+
+function Bear(type) {
+  this.type = type || 'any';
+}
+
+// bear prototype being created
+Bear.prototype.growl = function(says) {
+  return 'The ' + this.type + ' bear says ' + (says || 'grrr');
+};
